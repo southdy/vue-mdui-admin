aef4cc3a079ddea7da803a2caa6ae4498d9f3d0a
southdy <qiuhaifeng1>
2020-08-29 02:59:16 +0800

    ...

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ platform.description }}</h1>
   </div>
 </template>
+
+<script>
+import Platform from 'platform';
+
+export default {
+  name: 'About',
+  data() {
+    return {
+      platform: Platform,
+    };
+  },
+};
+</script>
